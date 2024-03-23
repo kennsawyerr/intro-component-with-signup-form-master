dbaafde5bf85d7e2b928741dab9701b9433fbd8f
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-03-23 23:23:59 +0100

    modified:   index.html 	modified:   index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+//declare vars
+
+const submitBtn = document.getElementById("submit");
+const  firstNameInput = document.getElementById("first-name")
+const  lastNameInput = document.getElementById("last-name")
+const  emailInput = document.getElementById("email")
+const  passwordInput = document.getElementById("password")
+//email input
+//last name input
+//name input
+//submit button
+// toggle ters and conditon
+
+
+
+
