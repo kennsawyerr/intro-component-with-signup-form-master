40b3f4c8915460a8643cd04234b96fc1fa7df9c9
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-03-27 22:44:26 +0100

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -14,72 +14,40 @@ const  passwordInput = document.getElementById("password")
 
 
 
-submitBtn.addEventListener("click", ()=> {
- if (firstNameInput && lastNameInput && emailInput && passwordInput === "" 
-    ){ return errorMessage("Pls fill the form to continue") }
-    else {
-        if (lastNameInput===""){
-errorMessage("please input your last names")}  ;
-if (firstNameInput===""){
-  errorMessage("please input your first name")};
-  if (passwordInput===""){
-    errorMessage("please input your password")}
-    if (emailInput===""){
-      errorMessage("please input your email address")} 
-        
-    }
-})
+submitBtn.addEventListener("click", handleClick)
 
-
-/*  
-// declare variables
-let inputVal = document.getElementById("inputVal");
-const subscribeBtn = document.getElementById("subBtn");
-const mainBdy = document.getElementById("main-bdy");
-const successMessage = document.getElementById("successMessage");
-const htmlMessageViewer = document.getElementById("email-message");
-
-subscribeBtn.addEventListener("click", () => {
-  let emailVal = inputVal.value;
-
-  if (emailVal === "") {
-    errorMessage("Please provide your email address");
-  } else {
-    const forbiddenArray = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (forbiddenArray.test(emailVal)) {
-      successNotificationMessage("Validation successful");
-      showSubscribeClip();
-    } else {
-      return errorMessage("Valid email required");
-    }
+function handleClick(event) {
+  event.preventDefault()
+  
+  if (firstNameInput.value ==="" && lastNameInput .value==="" && emailInput.value ==="" && passwordInput.value === "" 
+  ){ return errorMessage("Pls fill the form to continue")}
+  else {
+      if (lastNameInput.value===""){
+errorMessage("please input your last name")}  ;
+if (firstNameInput.value===""){
+errorMessage("please input your first name")};
+if (passwordInput.value===""){
+  errorMessage("please input your password")}
+  if (emailInput.value===""){
+    errorMessage("please input your email address")} 
+      
   }
-});
-
-const showSubscribeClip = () => {
-  mainBdy.style.display = "none";
-  successMessage.style.display = "block";
-};
-function errorMessage(info) {
-c+
 }
 
-const successNotificationMessage = (message) => {
-  htmlMessageViewer.innerHTML = `<p class="green">${message}</p>`;
-  inputVal.style.border = "1px solid green";
-  inputVal.style.backgroundColor = " rgb(182, 252, 182)";
-};
-
-
-
-const dismissBtn = () => {
-    mainBdy.style.display = "block";
-    successMessage.style.display = "none";
-  };
-
-*/
 
 function errorMessage (info) {
 
-  displayEl.textContent = info;
+  displayEl.innerHTML = `<p class="red">${info}</p>`;
 }
+
+
+
+
+
+
+
+
+
+
+
 
